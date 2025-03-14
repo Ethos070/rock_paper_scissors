7256c2f153d13d8dd916630cea486c7d685c1cc1
Alessandro <aledionisi05@gmail.com>
2025-03-14 17:27:45 +0100

    Write random computer choice algorithm

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-console.log('Hello World!');
+function getRandomInt(max){
+    // returns a random number between 0 and max (excluded)
+    return Math.floor(Math.random() * max);
+}
+
+function getComputerChoice(){
+    //generate a number from 0 to 2 (0, 1, 2)
+    randomNumber = getRandomInt(3);
+    if(randomNumber === 0){
+        return "rock";
+    }
+    else if(randomNumber === 1){
+        return "paper";
+    }
+    else{
+        return 'scissors';
+    }
+}
